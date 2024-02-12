6cfafd0aeb461383f22729a36c13bb1e10e65cd4
tkow <t.kusama0613@outlook.jp>
2024-02-12 23:45:06 +0900

    initial commit

--- jest/setup.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-export default () => {
-  const removeDir = path.join(process.cwd(), 'tmp')
-  if(fs.existsSync(removeDir)){
-    fs.rmSync(removeDir, { recursive: true, force: true });
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import { CommandFactory } from "nest-commander";
-import { CommandModule } from "./command.module";
+const DELEGATOR_NAME = '__delegator'
 
-export async function run() {
-  await CommandFactory.run(CommandModule, {
-    cliName: "zippee",
-    logger: ["warn", "error"],
-  });
+export const delegateProxy = <T extends object, P extends object>(self: T, delegateInstance: P, {
+    delegatorName = DELEGATOR_NAME
+} = {}): P => {
+    const proxy = new Proxy(delegateInstance as T & P, {
+        get: function (obj: P, prop: Extract<keyof P, string>) {
+            const target = obj[prop as keyof P]
+            if (typeof target === 'function') {
+                return (target as any).bind(obj);
+            } else {
+                return target
+            }
+        }
+    })
+    Object.assign(self, { [delegatorName]: proxy })
+    return proxy as P
 }
 
-export { CommandModule };
+type DelegateFunction<F> = F extends (...args: any) => any ? F : never
+type PickMatching<T, V> =
+    { [K in keyof T as T[K] extends V ? K : never]: T[K] }
+type OnlyFunctionKeys<P extends object> = keyof PickMatching<P, (...args: any) => any>
+type DelegateFunctionArg<P> = P extends (...args: infer A) => any ? A : any
+type DelegateFunctionRes<P> = P extends (...args: any) => infer R ? R : any
+
+
+export const delegate = <P extends object, K extends OnlyFunctionKeys<P> = OnlyFunctionKeys<P>>(id: K, options: { delegatorName?: string } = {}): DelegateFunction<P[K]> => {
+    const { delegatorName = DELEGATOR_NAME } = options
+
+    type Args = DelegateFunctionArg<P[K]>
+    type Res = DelegateFunctionRes<P[K]>
+
+    const delegateMethod = function (this: any, ...args: Args): Res {
+        return this[delegatorName][id](...args)
+    }
+
+    return delegateMethod as DelegateFunction<P[K]>
+}
